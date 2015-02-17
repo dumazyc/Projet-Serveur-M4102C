7d5db066dc4edc837eb6194fac2161ee1e9d7ac6
dumazyc <clement.dumazy@gmail.com>
2015-02-17 17:27:58 +0100

    question en cours

--- webserver/socket.c
+++ webserver/socket.c
@@ -47,7 +47,7 @@ int creer_serveur(int port){
 void traitement_signal ( int sig )
 {
 	if(sig==17){
-		printf ( "Client Déconnecté" );
+		printf ( "Client Déconnecté " );
 	}
 	printf ( "Signal %d reçu \n" , sig );
 	waitpid(-1, NULL, WNOHANG);			
